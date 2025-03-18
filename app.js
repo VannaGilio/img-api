5ecd6c6b71f2b3e5177d6aeb8bf1316a2dc47149
GIOVANNA COELHO GILIO DE SA <24122381@CFP127ALUNO.LOCAL>
2025-03-18 09:23:11 -0300

    js ok

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 'use strict'
 
-function aparecerFotos(img){
+async function carregarFotos() {
     const url = `https://picsum.photos/v2/list?page=2&limit=100`
-    const response = (url)
-     
-
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
 }
-function criarImagem(link){
+
+function criarImagem(foto) {
     const galeria = document.getElementById('galeria')
     const novaImg = document.createElement('img')
-    novaImg.src = link
-
+    novaImg.src = foto.download_url
     galeria.appendChild(novaImg)
 }
 
-function preencherFotos(){
+async function preencherFotos() {
+    const fotos = await carregarFotos()
+    const galeria = document.getElementById('galeria')
 
+    galeria.replaceChildren()
     fotos.forEach(criarImagem)
     console.log(fotos)
 }
+
+document.addEventListener('DOMContentLoaded', preencherFotos)
